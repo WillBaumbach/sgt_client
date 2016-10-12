790d20d85117d188c480c41a974ae1de37d6c15b
WilliamBaumbach1212 <william.baumbach@uconn.edu>
2016-10-12 15:49:22 -0400

    Initial

---
+++ desktop/src/com/willjs/sgt/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.willjs.sgt.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.willjs.sgt.SpaceGameThing;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SpaceGameThing(), config);
+	}
+}
