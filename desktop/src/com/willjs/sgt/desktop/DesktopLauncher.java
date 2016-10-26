b5800758ad2210e6f2dd44d1c6462684c1dbafa9
WilliamBaumbach1212 <william.baumbach@uconn.edu>
2016-10-26 11:49:52 -0400

    Added Inv Screen File and assets

--- desktop/src/com/willjs/sgt/desktop/DesktopLauncher.java
+++ desktop/src/com/willjs/sgt/desktop/DesktopLauncher.java
@@ -2,13 +2,13 @@ package com.willjs.sgt.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.willjs.sgt.Server;
+//import com.willjs.sgt.Server;
 import com.willjs.sgt.SpaceGameThing;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		
-		Server server = new Server("ws://localhost:1111");
+		//Server server = new Server("ws://localhost:1111");
 		
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.title = "Space Game Thing";
