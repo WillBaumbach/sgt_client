33568bec739452ebf4545142a57426963ad1016f
Jeffrey Santi <jeffreysanti@gmail.com>
2016-10-26 15:08:58 -0400

    fixed merge conflict

--- desktop/src/com/willjs/sgt/desktop/DesktopLauncher.java
+++ desktop/src/com/willjs/sgt/desktop/DesktopLauncher.java
@@ -4,24 +4,14 @@ import java.util.concurrent.TimeUnit;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-<<<<<<< HEAD
 import com.willjs.sgt.MessageListener;
 import com.willjs.sgt.Request;
 import com.willjs.sgt.Server;
-=======
-//import com.willjs.sgt.Server;
->>>>>>> b5800758ad2210e6f2dd44d1c6462684c1dbafa9
 import com.willjs.sgt.SpaceGameThing;
 
 public class DesktopLauncher {
 	
 	public static void main (String[] arg) {
-<<<<<<< HEAD
-=======
-		
-		//Server server = new Server("ws://localhost:1111");
-		
->>>>>>> b5800758ad2210e6f2dd44d1c6462684c1dbafa9
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.title = "Space Game Thing";
 		config.width = 800;
